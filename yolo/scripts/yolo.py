0854289a84dd02c05b23487d7b0c989d29841568
shadySource <shadySource2@gmail.com>
2017-07-18 18:16:43 -0400

    add compressed image repeater

--- yolo/scripts/yolo.py
+++ yolo/scripts/yolo.py
@@ -9,13 +9,14 @@ import cv2
 import tensorflow as tf
 import numpy as np
 from keras.layers import Input
+from keras.models import model_from_json
 from keras import backend as K
 import argparse
 
 import context
 
 from yad2k.utils.draw_boxes import draw_boxes
-from yad2k.models.keras_yolo import yolo_body, yolo_eval, yolo_head
+from yad2k.models.keras_yolo import CUSTOM_DICT, yolo_head_np, yolo_eval
 from subscriber import videosub
 
 
@@ -23,7 +24,7 @@ class yolo(object):
     '''
     YOLOv2 class integrated with YAD2K and ROS
     '''
-    def __init__(self, anchors_path, classes_path, weights_path, score_threshold=0.3, iou_threshold=0.6, max_detections=15):  
+    def __init__(self, json_path, anchors_path, classes_path, weights_path, score_threshold=0.3, iou_threshold=0.6, max_detections=15):  
         # Load classes and anchors.
         with open(classes_path) as f:
                 self.class_names = f.readlines()
@@ -39,42 +40,40 @@ class yolo(object):
 
         self.image_input = Input(shape=(None, None, 3))
 
-        self.yolo_body = yolo_body(self.image_input, len(self.anchors), len(self.class_names))
+        yolo_json_file = open(json_path, 'r')
+        yolo_json = yolo_json_file.read()
+        yolo_json_file.close()
+        self.yolo_model = model_from_json(yolo_json, CUSTOM_DICT)
+        
+        self.yolo_model.load_weights(weights_path)
 
-        self.yolo_body.load_weights(weights_path)
+        self.yolo_model.summary()
 
-        self.yolo_body.summary()
-
-        self.yolo_outputs = yolo_head(self.yolo_body.output, self.anchors, len(self.class_names))
-
-        self.input_image_shape = K.placeholder(shape=(2, ))
-
-        self.boxes, self.scores, self.classes = yolo_eval(
-            self.yolo_outputs,
-            self.input_image_shape,
-            max_boxes=max_detections,
-            score_threshold=score_threshold,
-            iou_threshold=iou_threshold)
+        self.max_boxes=max_detections,
+        self.score_threshold=score_threshold,
+        self.iou_threshold=iou_threshold
 
         print('yolo object created')
 
     def pred(self, image_s):
-        # Make predictions for one image, in (1, height, width, channel) format
+        # Make predictions for images in (num_images, height, width, channel) format
         assert len(image_s.shape) == 4 # image must have 4 dims ready to be sent into the graph
-        out_boxes, out_scores, out_classes = self.sess.run(
-                [self.boxes, self.scores, self.classes],
-                feed_dict={
-                    self.yolo_body.input: image_s,
-                    self.input_image_shape: [image_s.shape[1], image_s.shape[2]],
-                    K.learning_phase(): 0
-                })
+        # TODO allow multiple images at once.
+        assert image_s.shape[0] == 1
+        features = self.yolo_model.predict(image_s)
+
+        proc = yolo_head_np(features, self.anchors, len(self.class_names))
+
+        out_boxes, out_scores, out_classes = yolo_eval(proc, image_s.shape[1:3], max_boxes=self.max_boxes,
+            score_threshold=self.score_threshold, iou_threshold=self.iou_threshold)
+
         return out_boxes, out_scores, out_classes
 
     def display(self, out_boxes, out_scores, out_classes, image, name):
         if len(out_boxes) == 0:
-            cv2.imshow(name, np.floor(image[0] * 255 + 0.5).astype('uint8'))
+            cv2.imshow(name, np.floor(image * 255 + 0.5).astype('uint8'))
         else:
-            image = draw_boxes(image[0], out_boxes, out_classes, self.class_names, scores=out_scores)
+            image = draw_boxes(image, out_boxes, out_classes, self.class_names, scores=out_scores)
             cv2.imshow(name, image)
         cv2.waitKey(10)
 
@@ -82,13 +81,14 @@ def _main(args):
     anchors_path= os.path.expanduser(args.anchors_path)
     classes_path= os.path.expanduser(args.classes_path)
     weights_path = os.path.expanduser(args.weights_path)
+    json_path = os.path.expanduser(args.json_path)
 
     rospy.init_node("yoloNode")
 
-    yo = yolo(anchors_path, classes_path, weights_path, .4, .4, 100)
+    yo = yolo(json_path, anchors_path, classes_path, weights_path, .4, .4, 100)
 
     vid1 = videosub(args.first_topic)
-    if not (args.second_topic is None):
+    if args.second_topic is not None:
         vid2 = videosub(args.second_topic)
 
     rate = rospy.Rate(15)
@@ -98,11 +98,11 @@ def _main(args):
         if vid1.newImgAvailable:
             image = vid1.getProcessedImage()
             boxes, scores, classes = yo.pred(image)
-            yo.display(boxes, scores, classes, image, vid1.topic) # Display
-        if (not (args.second_topic is None)) and vid2.newImgAvailable:
+            yo.display(boxes, scores, classes, image[0], vid1.topic) # Display
+        if args.second_topic is not None and vid2.newImgAvailable:
             image = vid2.getProcessedImage()
             boxes, scores, classes = yo.pred(image)
-            yo.display(boxes, scores, classes, image, vid2.topic)
+            yo.display(boxes, scores, classes, image[1], vid2.topic)
 
         rate.sleep()
 
@@ -118,32 +118,38 @@ if __name__ == '__main__':
     argparser.add_argument(
         '-f',
         '--first_topic',
-        help="First topic to subscribe to. defaults to '/sensors/camera0/jpegbuffer'",
+        help="(optional)First topic to subscribe to. defaults to '/sensors/camera0/jpegbuffer'",
         default='/sensors/camera0/jpegbuffer')
 
     argparser.add_argument(
         '-s',
         '--second_topic',
-        help='Second topic to subscribe to. Leave blank if None.',
+        help='(optional) Second topic to subscribe to. Leave blank if None.',
         default=None)
 
     argparser.add_argument(
         '-c',
         '--classes_path',
-        help='path to classes file, defaults to YAD2K/model_data/aerial_classe.txt',
+        help='(optional)path to classes file, defaults to YAD2K/model_data/aerial_classe.txt',
         default=os.path.join(filepath, 'model_data', 'aerial_classes.txt'))
 
     argparser.add_argument(
         '-a',
         '--anchors_path',
-        help='path to anchors file, defaults to YAD2K/model_data/yolo_anchors.txt',
-        default=os.path.join(filepath, 'model_data', 'yolo_anchors.txt'))
+        help='(optional)path to anchors file, defaults to YAD2K/model_data/yolo_anchors.txt',
+        default=os.path.join(filepath, 'model_data', 'my_anchors.txt'))
     
     argparser.add_argument(
         '-w',
         '--weights_path',
-        help='path to model file, defaults to YAD2K/trained_stage_2_best.h5',
+        help='(optional) path to model weights file, defaults to YAD2K/trained_stage_2_best.h5',
         default=os.path.join(filepath, 'trained_stage_2_best.h5'))
+
+    argparser.add_argument(
+        '-j',
+        '--json_path',
+        help='(optional) path to model json file, defaults to YAD2K/trained_stage_2_best.h5',
+        default=os.path.join(filepath, 'model_data', 'yolo.json'))
 
     args = argparser.parse_args()
 

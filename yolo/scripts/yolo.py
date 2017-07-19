aa9f028c319774f5e1bf8a3294ecd4290ed190a4
shadySource <shadySource2@gmail.com>
2017-07-19 00:39:21 -0400

    add image publisher

--- yolo/scripts/yolo.py
+++ yolo/scripts/yolo.py
@@ -14,6 +14,9 @@ from keras.layers import Input
 from keras.models import model_from_json
 from keras import backend as K
 from std_msgs.msg import String
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 import context
 
@@ -27,7 +30,7 @@ class yolo(object):
     '''
     YOLOv2 class integrated with YAD2K and ROS
     '''
-    def __init__(self, paths, score_threshold=0.3, iou_threshold=0.6, max_detections=15, build=True):  
+    def __init__(self, paths, score_threshold=0.3, iou_threshold=0.6, max_detections=15):  
         # Load classes and anchors.
         json_path, anchors_path, classes_path, weights_path = paths
 
@@ -72,13 +75,13 @@ class yolo(object):
 
         return out_boxes, out_scores, out_classes
 
-def display(out_boxes, out_scores, out_classes, image, name, class_names):
-    if len(out_boxes) == 0:
-        cv2.imshow(name, image)
-    else:
+def draw(out_boxes, out_scores, out_classes, image, name, class_names, display=True):
+    if len(out_boxes) != 0:
         image = draw_boxes(image, out_boxes, out_classes, class_names, scores=out_scores, rectify=False)
+    if display:
         cv2.imshow(name, image)
-    cv2.waitKey(10)
+        cv2.waitKey(10)
+    return image
 
 def to_json(img_shape, boxes, scores, classes):
     # image_shape : the shape of the image used by yolo
@@ -106,36 +109,71 @@ def _main(args):
 
     rospy.init_node("yoloNode")
 
-    if args.mode.lower() == 'json':
-        pub1 = rospy.Publisher(args.first_topic + "/boxes", String, queue_size=10)
-        if args.second_topic is not None:
-            pub2 = rospy.Publisher(args.second_topic + "/boxes", String, queue_size=10)
-    else:
-        pass #multiarray publishers
-
     yo = yolo((json_path, anchors_path, classes_path, weights_path), .4, .4, 100)
 
+    if args.mode.lower()=='json':
+        box_type = String
+    else:
+        box_type = Float32MultiArray
+
+    bridge = CvBridge()
+
     vid1 = videosub(args.first_topic, (96, 96))
+    boxes_pub_1 = rospy.Publisher("/yolo/first/boxes", box_type, queue_size=10)
+    proc_image_pub_1 = rospy.Publisher("/yolo/first/proc_image", Image)
+
     if args.second_topic is not None:
         vid2 = videosub(args.second_topic)
+        boxes_pub_2 = rospy.Publisher("/yolo/second/boxes", box_type, queue_size=10)
+        proc_image_pub_2 = rospy.Publisher("/yolo/second/proc_image", Image)
 
+    
     rate = rospy.Rate(15)
     image = None
     while not rospy.is_shutdown():
         # Grab new images from the subscribers
         if vid1.newImgAvailable:
             image, image_data = vid1.getProcessedImage()
-            boxes, scores, classes = yo.pred(image_data)
-            if args.display:
-                display(boxes, scores, classes, image, vid1.topic, yo.class_names) # Display
-            if args.mode.lower()=='json':
-                pub1.publish(to_json((640, 480), boxes, scores, classes))
+            boxes, scores, classes = yo.pred(image_data, image.shape[0:2])
 
-        if args.second_topic is not None and vid2.newImgAvailable:
+            if args.display: #display the image
+                processed_image = draw(boxes, scores, classes, image, vid1.topic, yo.class_names) # Display
+
+            if args.mode.lower()=='json': # message is JSON
+                message = to_json(image.shape[0:2], boxes, scores, classes)
+            else: # message is multiarray
+                pass
+            boxes_pub_1.publish(message)
+
+            if args.publish: # Publish the processed image
+                if not args.display:
+                    processed_image = draw(boxes, scores, classes, image, vid1.topic, yo.class_names, False)
+                try:
+                    proc_image_pub_1.publish(bridge.cv2_to_imgmsg(processed_image, "bgr8"))
+                except CvBridgeError as e:
+                    print(e)
+
+
+        if args.second_topic is not None and vid2.newImgAvailable: #again, for second topic 
             image, image_data = vid2.getProcessedImage()
-            boxes, scores, classes = yo.pred(image_data)
+            boxes, scores, classes = yo.pred(image_data, image.shape[0:2])
+
             if args.display:
-                yo.display(boxes, scores, classes, image, vid2.topic)
+                processed_image = draw(boxes, scores, classes, image, vid1.topic, yo.class_names) # Display
+
+            if args.mode.lower()=='json':
+                message = to_json(image.shape[0:2], boxes, scores, classes)
+            else: # multiarray
+                pass
+            boxes_pub_2.publish(message)
+
+            if args.publish: # Publish the processed image
+                if not args.display:
+                    processed_image = draw(boxes, scores, classes, image, vid1.topic, yo.class_names, False)
+                try:
+                    proc_image_pub_2.publish(bridge.cv2_to_imgmsg(processed_image, "bgr8"))
+                except CvBridgeError as e:
+                    print(e)
 
         rate.sleep()
 
@@ -165,11 +203,19 @@ if __name__ == '__main__':
         '--display',
         action='store_true',
         help='use this flag to display an opencv image during computation')
+
+    argparser.add_argument(
+        '-p',
+        '--publish',
+        action='store_true',
+        help='use this flag to publish image with class boxes.')
     
     argparser.add_argument(
         '-m',
         '--mode',
-        help='mode: "json" for json publisher, else: multiarray publisher. Defaults to json.',
+        help='''mode: "json" for json publisher, else: multiarray publisher.
+        Defaults to json. Note: json publisher only publishes [classNum, centerX(%of image), cneterY(%of image), Confidence]
+        per box. Multiarray Publishes [classNum, y_min%, x_min%, y_max%, x_max%, confidence]''',
         default='json')
 
     argparser.add_argument(
